1e35508f5111a7af0aaa68132ccb9b21c51a412c
dede53 <hallomarcel.hm@gmail.com>
2017-01-10 22:07:05 +0100

    add arduino gpio and new settings

--- index.js
+++ index.js
@@ -1,51 +1,103 @@
-var dgram       	= require('dgram');
-var adapter 		= require('../../adapter-lib.js');
-var arduino 		= new adapter({
-	"name":"Arduino",
-	"loglevel":3,
-	"description": "sendet Befahle an den einen Arduino",
-	"settingsFile": "arduino.json",
-	"cron": ""
-});
+var dgram					=	require('dgram');
+var adapter					=	require('../../adapter-lib.js');
+var bodyParser				=	require('body-parser');
+var express					=	require('express.io');
+var app						=	express().http().io();
+var arduino					=	new adapter("arduino");
+var timeout					=	"";
 
-process.on('message', function(data){
-	var status = data.status;
-	var data = data.data;
-	if(data.protocol.includes(":")){
-		data.protocol = data.protocol.split(":");
-	}else{
-		data.protocol = [data.protocol];
-	}
-	switch(data.protocol[1]){
+process.on("message", function(data){
+	var data = JSON.parse(data);
+	arduino.log.debug(data);
+	switch(data.protocol){
+		case "setSetting":
+			arduino.setSetting(data.data);
+			break;
 		case "ir":
-			if(status == 1){
-				var msg = "sendIr:NEC:" + data.CodeOn + ":32::";
+			if(data.status == 1){
+				var msg = "sendIr:NEC:" + data.data.CodeOn + ":32::";
 			}else{
-				var msg = "sendIr:NEC:" + data.CodeOff + ":32::";
+				var msg = "sendIr:NEC:" + data.data.CodeOff + ":32::";
 			}
+			sendUDP(msg);
 			break;
 		case "344":
-			var msg = "send433:" + status + ":" + data.CodeOn + ":" + data.CodeOff + "::";
+			var msg = "send433:" + data.status + ":" + data.data.CodeOn + ":" + data.data.CodeOff + "::";
+			sendUDP(msg);
+			break;
+		case "pinAnalog":
+			var msg = "pinAnalog:" + data.data.CodeOn + ":" + data.status + "::";
+			sendUDP(msg);
+			break;
+		case "pinDigital":
+			var msg = "pinDigital:" + data.data.CodeOn + ":" + data.status + "::";
+			sendUDP(msg);
+			break;
+		default:
+			arduino.log.error("Problem mit dem Protocol:" + data.protocol);
 			break;
 	}
-	sendUDP(msg);
 });
 
 function sendUDP(msg) {
-	// var device = arduino.settings.arduinos[device.id];
-	// var client = dgram.createSocket('udp4'); // Neuen Socket zum Client aufbauen
-	// client.send(msg, 0, msg.length, device.port, device.ip, function(err, bytes) 
-	// {
-	// 	arduino.log.debug('udp://' + device.ip +':'+ device.port + "/" + msg); // Ausgabe der Nachricht
-	// 	client.close(); // Bei erfolgreichen Senden, die Verbindung zum CLient schließen
-	// });
 	arduino.settings.arduinos.forEach(function(device){
-		// dgram Klasse für UDP-Verbindungen
-		var client = dgram.createSocket('udp4'); // Neuen Socket zum Client aufbauen
+		var client = dgram.createSocket('udp4');
 		client.send(msg, 0, msg.length, device.port, device.ip, function(err, bytes) 
 		{
-			arduino.log.debug('udp://' + device.ip +':'+ device.port + "/" + msg); // Ausgabe der Nachricht
-			client.close(); // Bei erfolgreichen Senden, die Verbindung zum CLient schließen
+			arduino.log.debug('udp://' + device.ip +':'+ device.port + "/" + msg);
+			client.close();
 		});
 	});
+}
+
+app.use(bodyParser.json());									// for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));			// for parsing application/x-www-form-urlencoded
+app.get('/:id/:type/:pin/:value', function(req, res){
+	switch(req.params.type){
+		case 'digital':
+			if(req.params.value == 0){
+				arduino.setVariable("arduino." + req.params.id + ".digital." + req.params.pin, false);
+			}else{
+				arduino.setVariable("arduino." + req.params.id + ".digital." + req.params.pin, true);
+			}
+			break;
+		default:
+			arduino.setVariable("arduino." + req.params.id + ".analog." + req.params.pin, req.params.value);
+			break;
+	}
+	timeout = new Date().getTime();
+	res.json(200);
+});
+
+
+app.post('/setPin', function(req, res){
+	// console.log("Neue Daten vom Arduino!");
+	switch(req.body.type){
+		case 'digital':
+			if(req.body.value == 0){
+				arduino.setVariable("arduino." + req.body.id + ".digital." + req.body.pin, false);
+			}else{
+				arduino.setVariable("arduino." + req.body.id + ".digital." + req.body.pin, true);
+			}
+			break;
+		default:
+			arduino.setVariable("arduino." + req.body.id + ".analog." + req.body.pin, req.body.value);
+			break;
+	}
+	timeout = new Date().getTime();
+	res.json(req.body);
+});
+
+setInterval(function(){
+	if(timeout < (new Date().getTime() - 5 * 1000)){
+		process.send({statusMessage:"nicht verbunden!"});
+	}else{
+		process.send({statusMessage:"Verbunden"});
+	}
+}, 10 * 1000);
+
+try{
+	app.listen(arduino.settings.port);
+}catch(e){
+	arduino.log.error(e);
 }
